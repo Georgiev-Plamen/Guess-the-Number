807e8bae0294d58e0c309101dd5cb0895bb00817
Plamen Georgiev <plamen_gg@mail.bg>
2023-01-24 06:23:24 +0200

    added difficulty
    Added 5 attempts .

--- src/GuessTheNumber.java
+++ src/GuessTheNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class GuessTheNumber {
     public static void main(String[] args) {
-        Scanner scanner  = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         Random randomNumber = new Random();
         int computerNumber = randomNumber.nextInt(100);
@@ -11,47 +11,76 @@ public class GuessTheNumber {
         boolean itsNumber = false;
         int high = 0;
         int low = 0;
-        int attempt = 0;
+        int attempt = 5;
+        boolean itGuess = false;
 
-        while(!itsNumber) {
-            System.out.println("Guess the number [1-100]: ");
-            String playerInput = scanner.next();
+        while (attempt != 0) {
 
-
-            boolean isValid = true;
-
-            for (int i = 0; i < playerInput.length(); i++) {
-                if(playerInput.charAt(i) < 48 || playerInput.charAt(i) > 57) {
-                    isValid = false;
-                    System.out.println("Please write only number between 0 and 100!");
-                    System.out.println();
-                    break;
-                }
+            if (itGuess) {
+                break;
             }
 
-            if (isValid) {
-                int playerNumber = Integer.parseInt(playerInput);
-                attempt++;
+            for (int i = 1; i <= attempt; i++) {
 
-                if(playerNumber == computerNumber) {
-                    System.out.println("You guess it !");
+                String playerInput = "";
+
+                if (!itGuess) {
+                    System.out.printf("You have %d attempts%n", attempt);
+                    System.out.println("Guess the number [1-100]: ");
+                    playerInput = scanner.next();
+                } else {
                     break;
-                } else if (playerNumber > computerNumber) {
-                    System.out.println("Too High");
-                    high++;
-                } else if (playerNumber < computerNumber) {
-                    System.out.println("Too Low");
-                    low++;
+                }
+
+                boolean isValid = true;
+
+                for (int j = 0; j < playerInput.length(); j++) {
+                    if (playerInput.charAt(j) < 48 || playerInput.charAt(j) > 57) {
+                        isValid = false;
+                        System.out.println("Please write only number between 0 and 100!");
+                        System.out.println();
+                        break;
+                    }
+                }
+
+                if (isValid) {
+                    int playerNumber = Integer.parseInt(playerInput);
+                    attempt--;
+
+                    if (playerNumber == computerNumber) {
+                        System.out.println("You guess it !");
+                        itGuess = true;
+                        break;
+                    } else if (playerNumber > computerNumber) {
+                        System.out.println("Too High");
+                        high++;
+                    } else if (playerNumber < computerNumber) {
+                        System.out.println("Too Low");
+                        low++;
+                    }
                 }
             }
         }
 
-        System.out.printf("You guess a number on %d attempt%n", attempt);
-        if (high > 0) {
-            System.out.printf("You have %d result then is higher of computer number%n", high);
-        }
-        if ( low > 0) {
-            System.out.printf("You have %d result then is lower of computer number%n", low);
+
+        if (itGuess) {
+            System.out.printf("You guess a number on %d attempt%n", attempt);
+            if (high > 0) {
+                System.out.printf("You have %d result then is higher of computer number%n", high);
+            }
+            if (low > 0) {
+                System.out.printf("You have %d result then is lower of computer number%n", low);
+            }
+        } else {
+            System.out.println();
+            System.out.println("You lose !");
+            System.out.println();
+            if (high > 0) {
+                System.out.printf("You have %d result then is higher of the computer number%n", high);
+            }
+            if (low > 0) {
+                System.out.printf("You have %d result then is lower of the computer number%n", low);
+            }
         }
     }
 }
